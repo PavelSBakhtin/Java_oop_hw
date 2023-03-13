5f5c047ec7f6c4e9389ba898ba8176b4e9a0f474
PavelSB <1987PavelS@mail.ru>
2023-03-13 19:51:11 +0300

    home work 4 done

---
+++ homework_4/src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree<Human> familyTree = new FamilyTree<>();
+        familyTree.addFamilyTree(new Human("Алекс", "муж", 27));
+        familyTree.addFamilyTree(new Human("Владимир Владимирович Путин", "муж", 70));
+        familyTree.getByName("алекс");
+    }
+}
---
+++ homework_4/src/comporator/AnimalIterator.java
@@ -0,0 +1,25 @@
+package src.comporator;
+
+import java.util.Iterator;
+import java.util.List;
+
+import src.Animal;
+
+public class AnimalIterator<T extends Animal> implements Iterator<T> {
+    private int index;
+    private List<T> humans;
+
+    public AnimalIterator(List<T> humans) {
+        this.humans = humans;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return humans.size() > index;
+    }
+
+    @Override
+    public T next() {
+        return humans.get(index++);
+    }
+}
